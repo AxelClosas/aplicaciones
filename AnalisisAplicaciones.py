ec01fd59fccbeb742c6ef82abf62f4ce2dfe4023
AxelClosas <96825620+AxelClosas@users.noreply.github.com>
2023-11-10 11:12:55 -0300

    Update AnalisisAplicaciones.py

--- AnalisisAplicaciones.py
+++ AnalisisAplicaciones.py
@@ -21,14 +21,21 @@ class AnalisisAplicaciones():
         filtro_unica = []
         filtro_unica.extend(list(filter(lambda item: 'Unica' in item['DOSIS'], self.lista_de_vacunas)))
         return filtro_unica
-    
+
+    def filtrar_dosis_adicional(self) -> list:
+        filtro_adicional = []
+        filtro_adicional.extend(list(filter(lambda item: 'Adicional' in item['DOSIS'], self.lista_de_vacunas)))
+        return filtro_adicional
+        
     def obtener_aplicaciones(self, nombre='aplicaciones_totales'):
         primera_dosis = len(self.filtrar_primera_dosis())
         segunda_dosis = len(self.filtrar_segunda_dosis())
         unica_dosis = len(self.filtrar_dosis_unica())
+        dosis_adicional = len(self.filtrar_dosis_adicional())
+        
         print(f'Exportando archivo: {nombre}')
         with open(f'{nombre}.txt', 'w') as file:
-            file.write(f'Primera dosis: {primera_dosis}\nSegunda dosis: {segunda_dosis}\nUnica dosis: {unica_dosis}\n')
+            file.write(f'Primera dosis: {primera_dosis}\nSegunda dosis: {segunda_dosis}\nUnica dosis: {unica_dosis}\nDosis Adicional: {dosis_adicional}')
         refuerzos = AnalisisRefuerzo(self.lista_de_vacunas)
         refuerzos.obtener_refuerzos()
         
