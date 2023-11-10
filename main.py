066df64dfd9e94ce0286b3810348c14971f61529
Axel Closas <axelclosas13@gmail.com>
2023-11-10 10:31:37 -0300

    Manejo de errores. Implementación de menú de opciones.

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-import Classes.AnalisisRefuerzo as AnalisisRefuerzo
+from Aplicaciones import Aplicaciones
+from AnalisisRefuerzo import AnalisisRefuerzo
+from AnalisisAplicaciones import AnalisisAplicaciones
+
 
 def run():
     try:
+        vacunas = Aplicaciones()
+        lista = vacunas.exportar_lista_vacunas()
         menu = '''
     ** MENÚ **
 
 1. Obtener cantidad de refuerzos aplicados por vacuna
 2. Obtener tipo y cantidad de refuerzos aplicados
-
-    Opción: 
-'''
-        print('Hola!, se están leyendo los archivos .csv ubicados en la carpeta CSV. Ten paciencia, esto puede demorar unos segundos.')
-        refuerzos = AnalisisRefuerzo.AnalisisRefuerzo()
+3. Obtener total de aplicaciones. (1ra dosis, 2da dosis)
+    Opción: '''
         op = int(input(menu))
         match op:
             case 1:
+                refuerzos = AnalisisRefuerzo(lista)
                 refuerzos.refuerzos_por_vacuna()
             case 2:
+                refuerzos = AnalisisRefuerzo(lista)
                 refuerzos.obtener_refuerzos()
+            case 3:
+                aplicaciones = AnalisisAplicaciones(lista)
+                aplicaciones.obtener_aplicaciones()
             case _:
                 raise Exception('Opción no encontrada. Intenta nuevamente...')
         # refuerzo = refuerzos.filtrar_refuerzos()
         # print(refuerzo[0])
     except Exception as e:
-        print(e.name)
+        print(e)
 
 if __name__ == '__main__':
     run()
