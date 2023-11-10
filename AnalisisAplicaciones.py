a0a24bd01cf2fe62c82d5ae55585418837a2abfc
Axel Closas <axelclosas13@gmail.com>
2023-11-10 10:33:15 -0300

    Refactoreo de clases para recibir una lista de vacunas y en función de ella trabajar con la lógica de negocio.

---
+++ AnalisisAplicaciones.py
@@ -0,0 +1,34 @@
+from AnalisisRefuerzo import AnalisisRefuerzo
+
+class AnalisisAplicaciones():
+    def __init__(self, lista_de_vacunas):
+        self.lista_de_vacunas = lista_de_vacunas
+        self.primera_dosis = self.filtrar_primera_dosis()
+        self.primera_dosis = self.filtrar_primera_dosis()
+        self.primera_dosis = self.filtrar_primera_dosis()
+
+    def filtrar_primera_dosis(self) -> list:
+        filtro_primera = []
+        filtro_primera.extend(list(filter(lambda item: '1ra' in item['DOSIS'], self.lista_de_vacunas)))
+        return filtro_primera
+    
+    def filtrar_segunda_dosis(self) -> list:
+        filtro_segunda = []
+        filtro_segunda.extend(list(filter(lambda item: '2da' in item['DOSIS'], self.lista_de_vacunas)))
+        return filtro_segunda
+    
+    def filtrar_dosis_unica(self) -> list:
+        filtro_unica = []
+        filtro_unica.extend(list(filter(lambda item: 'Unica' in item['DOSIS'], self.lista_de_vacunas)))
+        return filtro_unica
+    
+    def obtener_aplicaciones(self, nombre='aplicaciones_totales'):
+        primera_dosis = len(self.filtrar_primera_dosis())
+        segunda_dosis = len(self.filtrar_segunda_dosis())
+        unica_dosis = len(self.filtrar_dosis_unica())
+        print(f'Exportando archivo: {nombre}')
+        with open(nombre, 'w') as file:
+            file.write(f'Primera dosis: {primera_dosis}\nSegunda dosis: {segunda_dosis}\nUnica dosis: {unica_dosis}\n')
+        refuerzos = AnalisisRefuerzo(self.lista_de_vacunas)
+        refuerzos.obtener_refuerzos()
+        
