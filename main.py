102bbec59b8bcba16da6d229c6fcfa45289417fc
Axel Closas <axelclosas13@gmail.com>
2023-11-13 10:45:08 -0300

    Actualización de script principal. Ya no cuenta con un menú para elegir que se desea exportar. Genera un reporte automatizado de datos importantes.

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 from Aplicaciones import Aplicaciones
 from AnalisisRefuerzo import AnalisisRefuerzo
 from AnalisisAplicaciones import AnalisisAplicaciones
+from GenerarReporte import GenerarReporte
 import time
 
 
 def run():
-    try:
-        vacunas = Aplicaciones()
-        lista = vacunas.exportar_lista_vacunas()
-        menu = """
-    ** MENÚ **
-1. Obtener cantidad de refuerzos aplicados por vacuna
-2. Obtener tipo y cantidad de refuerzos aplicados
-3. Obtener total de aplicaciones.
-    Opción: """
-        op = input(menu)
-        match op:
-            case 1:
-                refuerzos = AnalisisRefuerzo(lista)
-                refuerzos.refuerzos_por_vacuna()
-            case 2:
-                refuerzos = AnalisisRefuerzo(lista)
-                refuerzos.obtener_refuerzos()
-            case 3:
-                aplicaciones = AnalisisAplicaciones(lista)
-                aplicaciones.obtener_aplicaciones()
-            case _:
-                raise Exception("Opción no encontrada. Intenta nuevamente...")
-        # refuerzo = refuerzos.filtrar_refuerzos()
-        # print(refuerzo[0])
-    except Exception as e:
-        print(e)
+    vacunas = Aplicaciones()
+    lista = vacunas.exportar_lista_vacunas()
+    aplicaciones = AnalisisAplicaciones(lista)
+    refuerzos = AnalisisRefuerzo(lista)
+    reporte = GenerarReporte(aplicaciones, refuerzos)
+    reporte.generar_reporte_de_dosis_aplicadas_y_refuerzos()
 
 
 if __name__ == "__main__":
