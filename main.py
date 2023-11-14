71f3a6aef820b64b3c656e2e1d6e24e795ec0e2b
Axel Closas <axelclosas13@gmail.com>
2023-11-14 10:29:34 -0300

    Punto de entrada de la aplicación creado en función de si existe o no la base de datos completa dentro de la carpeta CSV.

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import app.ProcesosLogica as PL
-from app.AnalisisRefuerzo import AnalisisRefuerzo
-from app.AnalisisAplicaciones import AnalisisAplicaciones
-from app.GenerarReporte import GenerarReporte
-
+import app.AnalisisRefuerzo as AR
+import app.AnalisisAplicaciones as AP
+import app.GenerarReporte as GR
 import time
+import os
 
 
 def run():
-    # Ejecuta la función que se encuentre en ProcesosLogica
-    # PL.desempaquetadoDeComprimidoZIP()
-    pass
-    # aplicaciones = AnalisisAplicaciones(lista)
-    # refuerzos = AnalisisRefuerzo(lista)
-    # reporte = GenerarReporte(aplicaciones, refuerzos)
-    # reporte.generar_reporte_de_dosis_aplicadas_y_refuerzos()
+    ruta_carpeta_csv = PL.generar_ruta_carpeta_csv(carpeta_csv="CSV")
+    nombre_archivo_base_completa = "BaseCompletaCOVID.csv"
+    es_windows = PL.sistema_actual()
+    if es_windows:
+        ruta_completa_base_covid = f"{ruta_carpeta_csv}\\{nombre_archivo_base_completa}"
+    else:
+        ruta_completa_base_covid = f"{ruta_carpeta_csv}/{nombre_archivo_base_completa}"
+    base_existe = False
+
+    with os.scandir(ruta_carpeta_csv) as ficheros:
+        for fichero in ficheros:
+            if nombre_archivo_base_completa == fichero.name:
+                base_existe = True
+                break
+
+    if base_existe:
+        print(f"Archivo {nombre_archivo_base_completa} existente.")
+        print(ruta_completa_base_covid)
+    else:
+        print(
+            "\nHola!, en unos segundos iniciará el proceso de descompresión del archivo principal que contiene las bases de datos..."
+        )
+        time.sleep(2)
+        PL.desempaquetadoDeComprimidoZIP()
+        print("Uniendo archivos para generar base de datos completa")
+        PL.creacionDeBaseDeDatosCompletaCOVID()
+        print(
+            "Parece que todo salió bien!, ejecuta nuevamente el script para obtener los datos que precises. Hasta pronto!"
+        )
+        time.sleep(2)
+        exit()
 
 
 if __name__ == "__main__":
