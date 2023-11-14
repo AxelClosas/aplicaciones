648dc7507fae0e563a7ca83a6898fa6874af8e58
Axel Closas <axelclosas13@gmail.com>
2023-11-14 10:26:21 -0300

    Se agregaron funciones para ser reutilizadas en distintas partes del código. Se crearon funciones que contienen diferentes procesos para llevar a cabo las tareas de descompresión y generación de la base de datos completa de COVID.

--- app/ProcesosLogica.py
+++ app/ProcesosLogica.py
@@ -1,18 +1,53 @@
-from app.Descomprimir import Descomprimir
+import app.Aplicaciones as APL
+import app.Descomprimir as Des
+
 from platform import system
+import os
+import csv
 
 
 def desempaquetadoDeComprimidoZIP():
-    try:
-        d = Descomprimir()
-        d.descomprimir()
-        d.mover_archivos_csv()
-        d.limpieza_de_directorio()
-    except:
-        pass
+    descompresor = Des.Descomprimir()
+    print("Descomprimiendo archivo...")
+    descompresor.descomprimir()
+    print("Moviendo archivos...")
+    descompresor.mover_archivos_csv()
+    print("Limpiando directorio...")
+    descompresor.limpieza_de_directorio()
+
+
+def creacionDeBaseDeDatosCompletaCOVID():
+    aplicaciones = APL.Aplicaciones()
+    aplicaciones.exportar_lista_vacunas()
 
 
 def sistema_actual():
     # Retorna True si es Windows o False si no lo es
     sistema = system()
     return True if sistema == "Windows" else False
+
+
+def generar_ruta_carpeta_csv(carpeta_csv):
+    # Recibe True si es Windows o False si no lo és
+    es_windows = sistema_actual()
+    directorio_actual = os.getcwd()
+    if es_windows:
+        ruta_carpeta_csv = f"{directorio_actual}\\{carpeta_csv}"
+    else:
+        ruta_carpeta_csv = f"{directorio_actual}/{carpeta_csv}"
+    return ruta_carpeta_csv
+
+    # Función que procesa la lectura y guardado de un archivo csv
+
+
+def read_csv(path) -> list:
+    with open(path, "r", encoding="latin-1") as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        header = next(reader)
+        data = []
+        for row in reader:
+            iterable = zip(header, row)
+            vacuna_aplicada = {key: value for key, value in iterable}
+            data.append(vacuna_aplicada)
+
+    return data
