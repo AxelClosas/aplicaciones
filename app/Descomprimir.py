2f464ee972b56c07751e4702241779ea506dbb23
Axel Closas <axelclosas13@gmail.com>
2023-11-14 10:24:37 -0300

    Se hace uso de las funciones en ProcesosLogica.py para obtener el tipo de sistema y la ruta de la carpeta csv a analizar pertinente. Cumple sus tareas correctamente de descompresión, movimiento de archivos y limpieza de directorio.

--- app/Descomprimir.py
+++ app/Descomprimir.py
@@ -2,38 +2,46 @@ import app.ProcesosLogica as PL
 import zipfile
 import os
 import shutil
-from platform import system
 
 
 class Descomprimir:
-    def __init__(self):
-        self.sistema = system()
+    def __init__(self, carpeta_csv="CSV"):
+        self.carpeta_csv = carpeta_csv
 
-    def descomprimir(self):
+    def descomprimir(self, nombre_comprimido="CATAMARCA.zip"):
         try:
-            mi_archivo = "CATAMARCA.zip"
             nombre_de_archivos_comprimidos = []
-            carpeta_csv = PL.directorio_actual_segun_sistema()
+            # Genera ruta de carpeta CSV si es Windows o Linux
+            es_windows = PL.sistema_actual()
+            ruta_carpeta_csv = PL.generar_ruta_carpeta_csv(self.carpeta_csv)
             # Descomprimir primer archivo "CATAMARCA.zip"
-            print(f"Descomprimiendo archivo {mi_archivo}")
-            with zipfile.ZipFile(f"{carpeta_csv}/{mi_archivo}", "r") as file:
-                file.extractall(path=carpeta_csv)
+            if es_windows:
+                ruta_comprimido = f"{ruta_carpeta_csv}\\{nombre_comprimido}"
+                ruta_nombre_archivo = f"{ruta_carpeta_csv}\\{nombre_archivo}"
+            else:
+                ruta_comprimido = f"{ruta_carpeta_csv}/{nombre_comprimido}"
+                ruta_nombre_archivo = f"{ruta_carpeta_csv}/{nombre_archivo}"
+
+            print(f"Descomprimiendo archivo {nombre_comprimido}")
+
+            with zipfile.ZipFile(ruta_comprimido, "r") as file:
+                file.extractall(path=ruta_carpeta_csv)
             # Escanera directorio para recopilar nombres de los archivos que extraje desde CATAMARCA.zip
-            with os.scandir(carpeta_csv) as ficheros:
+            with os.scandir(ruta_carpeta_csv) as ficheros:
                 for fichero in ficheros:
                     # Comprueba que el nombre del archivo no sea CATAMARCA.zip y que contenga .zip dentro de su nombre y lo agrega a una lista de nombres
-                    if mi_archivo != fichero.name and ".zip" in fichero.name:
+                    if nombre_comprimido != fichero.name and ".zip" in fichero.name:
                         nombre_de_archivos_comprimidos.append(fichero.name)
             # Recorre cada nombre para descomprimir los archivos
             for nombre_archivo in nombre_de_archivos_comprimidos:
                 print(f"Descomprimiendo archivo {nombre_archivo}")
-                with zipfile.ZipFile(f"{carpeta_csv}/{nombre_archivo}", "r") as file:
-                    file.extractall(path=carpeta_csv)
+                with zipfile.ZipFile(ruta_nombre_archivo, "r") as file:
+                    file.extractall(path=ruta_carpeta_csv)
         except zipfile.BadZipFile:
             print("Error: Archivo Zip corrupto.")
 
     def mover_archivos_csv(self):
-        carpeta_csv = directorio_actual_segun_sistema()
+        carpeta_csv = PL.generar_ruta_carpeta_csv(self.carpeta_csv)
         carpetas = []
         with os.scandir(carpeta_csv) as ficheros:
             for fichero in ficheros:
@@ -46,16 +54,17 @@ class Descomprimir:
                     shutil.move(fichero, f"{carpeta_csv}")
 
     def limpieza_de_directorio(self):
-        carpeta_csv = directorio_actual_segun_sistema()
+        carpeta_csv = PL.generar_ruta_carpeta_csv(self.carpeta_csv)
+        es_windows = PL.sistema_actual()
 
         print(f"Limpiando directorio {carpeta_csv}")
         with os.scandir(carpeta_csv) as ficheros:
             for fichero in ficheros:
                 if ".zip" in fichero.name:
-                    if self.sistema != "Windows":
-                        os.remove(f"{carpeta_csv}/{fichero.name}")
-                    else:
+                    if es_windows:
                         os.remove(f"{carpeta_csv}\\{fichero.name}")
+                    else:
+                        os.remove(f"{carpeta_csv}/{fichero.name}")
                 if os.path.isdir(fichero):
                     os.rmdir(fichero)
 
