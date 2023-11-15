102381c64d52b1d94f5dd2854710022ef4068e17
Axel Closas <axelclosas13@gmail.com>
2023-11-15 13:01:54 -0300

    Simplificando la función run(). Se implementan procesos previamente definidos en la clase LogicaNegocio.py para una mejor legibilidad del código. Se imprimen por pantalla unos ejemplos.

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import app.ProcesosLogica as PL
-import app.AnalisisRefuerzo as AR
 import app.AnalisisAplicaciones as AP
-import app.GenerarReporte as GR
+import app.LogicaNegocio as LN
 import time
 import os
 
 
 def run():
-    ruta_carpeta_csv = PL.generar_ruta_carpeta_csv(carpeta_csv="CSV")
-    nombre_archivo_base_completa = "BaseCompletaCOVID.csv"
-    es_windows = PL.sistema_actual()
-    if es_windows:
-        ruta_completa_base_covid = f"{ruta_carpeta_csv}\\{nombre_archivo_base_completa}"
-    else:
-        ruta_completa_base_covid = f"{ruta_carpeta_csv}/{nombre_archivo_base_completa}"
-    base_existe = False
+    (
+        base_existe,
+        ruta_completa_base_covid,
+    ) = LN.consultarExistenciaDeBaseDeDatosCompletaCOVID()
 
-    with os.scandir(ruta_carpeta_csv) as ficheros:
-        for fichero in ficheros:
-            if nombre_archivo_base_completa == fichero.name:
-                base_existe = True
-                break
     # Si la base ya existe, aquí armamos el menú
     if base_existe:
-        lista_de_vacunas = PL.read_csv(ruta_completa_base_covid)
-        print(lista_de_vacunas[4000:4030])
+        lista_de_vacunas_completa = PL.read_csv(ruta_completa_base_covid)
+        lista_de_vacunas_catamarca = LN.procesoObtenerListaDeVacunasDeCatamarca(
+            lista_de_vacunas_completa
+        )
+
+        analisis_aplicaciones = AP.AnalisisAplicaciones(
+            lista_de_vacunas_completa, lista_de_vacunas_catamarca
+        )
+
+        print("Total dosis: ", analisis_aplicaciones.total_aplicaciones())
+        print(
+            "Total dosis Catamarca: ",
+            analisis_aplicaciones.total_aplicaciones_catamarca(),
+        )
 
     else:
         try:
@@ -32,16 +34,17 @@ def run():
                 "\nHola!, en unos segundos iniciará el proceso de descompresión del archivo principal que contiene las bases de datos..."
             )
             time.sleep(2)
-            PL.desempaquetadoDeComprimidoZIP()
+            LN.desempaquetadoDeComprimidoZIP()
             print("Uniendo archivos para generar base de datos completa")
-            PL.creacionDeBaseDeDatosCompletaCOVID()
+            LN.creacionDeBaseDeDatosCompletaCOVID()
             print(
                 "Parece que todo salió bien!, ejecuta nuevamente el script para obtener los datos que precises. Hasta pronto!"
             )
             time.sleep(2)
             exit()
+
         except FileNotFoundError as e:
-            PL.creacionDeBaseDeDatosCompletaCOVID()
+            LN.creacionDeBaseDeDatosCompletaCOVID()
             print(
                 "Parece que todo salió bien!, ejecuta nuevamente el script para obtener los datos que precises. Hasta pronto!"
             )
