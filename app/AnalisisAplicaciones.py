a6203bb7921865476577287fd94b2a3c210d6d83
Axel Closas <axelclosas13@gmail.com>
2023-11-16 12:50:09 -0300

    Se creo una función que obtiene las aplicaciones por departamento.

--- app/AnalisisAplicaciones.py
+++ app/AnalisisAplicaciones.py
@@ -2,6 +2,8 @@ import app.ProcesosLogica as PL
 import csv
 
 
+# [1,2,3,4,5,6]
+# ["axel" => "closas" ]
 class AnalisisAplicaciones:
     def __init__(self, lista_de_vacunas_completa, lista_de_vacunas_catamarca):
         self.lista_de_vacunas_completa = lista_de_vacunas_completa
@@ -12,6 +14,35 @@ class AnalisisAplicaciones:
 
     def total_aplicaciones_catamarca(self) -> int:
         return len(self.lista_de_vacunas_catamarca)
+
+    def total_aplicaciones_por_departamento_csv(
+        self,
+        nombre_archivo="aplicaciones_por_id_depto.csv",
+        carpeta_destino="Resultados",
+    ) -> csv:
+        deptos = PL.obtener_id_departamentos_desde_archivo()
+        frecuencia = {}
+        resultado = {}
+        print("Analizando aplicaciones por id de departamento...")
+        for item in self.lista_de_vacunas_catamarca:
+            if int(item["ID_DEPTO_DOMICILIO"]) in frecuencia.keys():
+                frecuencia[int(item["ID_DEPTO_DOMICILIO"])] += 1
+            else:
+                frecuencia[int(item["ID_DEPTO_DOMICILIO"])] = 1
+
+        resultado = {
+            deptos[key]: frecuencia[key]
+            for key in frecuencia.keys()
+            if key in deptos.keys()
+        }
+
+        if frecuencia and resultado:
+            with open(
+                f"{carpeta_destino}/{nombre_archivo}", "w", encoding="latin-1"
+            ) as csvfile:
+                csvfile.write("Provincia;Cantidad\n")
+                for key, value in resultado.items():
+                    csvfile.write(f"{key};{value}\n")
 
     def filtrar_primera_dosis(self) -> list:
         filtro_primera = []
