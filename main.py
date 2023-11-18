91fd3bdcd94b64a9d1d0576202e0224670ca8341
Axel Closas <axelclosas13@gmail.com>
2023-11-18 17:26:42 -0300

    Se documento un poco el modulo para una mejor comprensión, falta mucho.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+# Se importan los modulos necesarios para ejecutar los principales procesos del Programa
 import app.ProcesosLogica as PL
-import app.AnalisisAplicaciones as AP
 import app.LogicaNegocio as LN
 import time
-import os
 from menu import menu
 
 
+# Función llamada al ejecutar el Programa como script
 def run():
+    # Primero se ejecuta el proceso que consulta si existe la Base de Datos Completa de vacunas COVID
+    # Se guarda en base_existe True o False dependiendo si existe o no la base en cuestión
+    # Se guarda en ruta_completa_base_covid None si la base no existe, caso contrario, se guarda la ruta absoluta hacia el archivo de la base de datos completa para ser utilizada más adelante
     (
         base_existe,
         ruta_completa_base_covid,
     ) = LN.consultarExistenciaDeBaseDeDatosCompletaCOVID()
 
-    # Si la base ya existe, aquí armamos el menú
+    # Se comprueba si la base existe para desarrollar la lógica principal
     if base_existe:
         lista_de_vacunas_completa = PL.read_csv(ruta_completa_base_covid)
         lista_de_vacunas_catamarca = LN.procesoObtenerListaDeVacunasDeCatamarca(
@@ -41,18 +44,24 @@ def run():
                 print(
                     "Ups... El número que ingresaste no se encuentra en el menú. Por favor, elije otro."
                 )
+    # En caso de no existir, se busca obtener los archivos necesarios para crear la Base de Datos
     else:
+        # Con el bloque try except capturamos potencionales errores como el no encontrar los archivos necesarios
         try:
             print(
                 "\nHola!, en unos segundos iniciará el proceso de descompresión del archivo principal que contiene las bases de datos..."
             )
             time.sleep(2)
+            # Se ejecuta el proceso de descomprimir el archivo descargado desde la nube
             LN.desempaquetadoDeComprimidoZIP()
+
+            # Terminado el proceso de desempaquetado. Se inicia la ejecución del proceso de creación de la Base de Datos
             print("Uniendo archivos para generar base de datos completa")
             LN.creacionDeBaseDeDatosCompletaCOVID()
             print(
                 "Parece que todo salió bien!, ejecuta nuevamente el script para obtener los datos que precises. Hasta pronto!"
             )
+            # Esperamos 2 segundos antes de cortar la ejecución del Programa
             time.sleep(2)
             exit()
 
@@ -65,6 +74,7 @@ def run():
             exit()
 
 
+# Implementamos el condicional para poder ejecutar el programa desde la Consola de Comandos
 if __name__ == "__main__":
     inicio = time.time()
     run()
