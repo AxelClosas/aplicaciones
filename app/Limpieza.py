dbd1aaaaaa89c9f77283a4cf82779bd5e283385a
Axel Closas <axelclosas13@gmail.com>
2023-11-18 17:22:48 -0300

    Nuevas clases integradas. Se independizó la lógica que se encontraba en el modulo Descomprimir

---
+++ app/Limpieza.py
@@ -0,0 +1,23 @@
+import app.ProcesosLogica as PL
+import os
+from app.Configuraciones import nombre_carpeta_csv
+
+
+class Limpieza:
+    def __init__(self, carpeta_csv=nombre_carpeta_csv):
+        self.carpeta_csv = nombre_carpeta_csv
+
+    def limpieza_de_directorio(self):
+        carpeta_csv = PL.generar_ruta_carpeta_csv(self.carpeta_csv)
+        es_windows = PL.sistema_actual()
+
+        print(f"Limpiando directorio {carpeta_csv}")
+        with os.scandir(carpeta_csv) as ficheros:
+            for fichero in ficheros:
+                if ".zip" in fichero.name:
+                    if es_windows:
+                        os.remove(f"{carpeta_csv}\\{fichero.name}")
+                    else:
+                        os.remove(f"{carpeta_csv}/{fichero.name}")
+                if os.path.isdir(fichero):
+                    os.rmdir(fichero)
---
+++ app/MoverArchivos.py
@@ -0,0 +1,22 @@
+import app.ProcesosLogica as PL
+import os
+import shutil
+from app.Configuraciones import nombre_carpeta_csv
+
+
+class MoverArchivos:
+    def __init__(self, carpeta_csv=nombre_carpeta_csv):
+        self.carpeta_csv = nombre_carpeta_csv
+
+    def mover_archivos_csv(self):
+        carpeta_csv = PL.generar_ruta_carpeta_csv(self.carpeta_csv)
+        carpetas = []
+        with os.scandir(carpeta_csv) as ficheros:
+            for fichero in ficheros:
+                if os.path.isdir(fichero):
+                    carpetas.append(fichero.name)
+        for carpeta in carpetas:
+            with os.scandir(f"{carpeta_csv}/{carpeta}") as ficheros:
+                for fichero in ficheros:
+                    print(f"Moviendo archivo {fichero} a {carpeta_csv}")
+                    shutil.move(fichero, f"{carpeta_csv}")
