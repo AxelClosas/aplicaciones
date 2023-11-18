ba28a3af36907c3bff752c70ce801d8af9f3de02
Axel Closas <axelclosas13@gmail.com>
2023-11-18 17:20:37 -0300

    Actualización de archivo Configuraciones

--- app/Configuraciones.py
+++ app/Configuraciones.py
@@ -1,3 +1,7 @@
 # Nombre de archivos en DocumentosClave
 documentos_clave = "DocumentosClave"
 archivo_id_departamentos = "id_departamentos.csv"
+
+# Nommbre de archivos en CSV
+nombre_archivo_comprimido_principal = "CATAMARCA.zip"
+nombre_carpeta_csv = "CSV"
