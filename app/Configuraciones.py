427002ec185ce498b5fcf7281f4cf385d1988749
Axel Closas <axelclosas13@gmail.com>
2023-11-18 18:33:42 -0300

    Actualización de variables

--- app/Configuraciones.py
+++ app/Configuraciones.py
@@ -2,6 +2,7 @@
 documentos_clave = "DocumentosClave"
 archivo_id_departamentos = "id_departamentos.csv"
 
-# Nommbre de archivos en CSV
-nombre_archivo_comprimido_principal = "CATAMARCA.zip"
+# Nombre de archivos en CSV
 nombre_carpeta_csv = "CSV"
+nombre_archivo_comprimido_principal = "CATAMARCA.zip"
+nombre_archivo_base_completa = "BaseCompletaCOVID.csv"
