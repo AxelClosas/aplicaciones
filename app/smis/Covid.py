70fbcbd68ca9a1d59ef71c9f6c8e474effbca51d
Axel Closas <axelclosas13@gmail.com>
2023-12-28 00:17:52 -0300

    Refactor de clase para disminuir el hardcode dentro de la misma

--- app/smis/Covid.py
+++ app/smis/Covid.py
@@ -1,3 +1,6 @@
+from app.Configuraciones import diccionario_de_vacunas
+
+
 class Covid:
     def __init__(self, movimientos: list):
         # Al instanciar la clase Covid, recibimos los movimientos para procesarlos luego
@@ -8,75 +11,10 @@ class Covid:
     # Procesamos los datos para obtener los movimientos estandarizados y solamente de vacunas
     def procesar_datos(self) -> list:
         def cambiar_nombre(item) -> dict:
-            cambiar_nombre = {
-                "Astrazeneca": [
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - WUXI - UNIVERSAL",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - LIOM",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - MABX- BIO- AMY",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 8 DOSIS - AB - PHARM",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - SKB CAT",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - AZ - AMYLIN - AZ CAN",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - BV",
-                    "COVID 19 VACCINE ASTRAZENECA (CHADOX1-S RECOMBINANTE) - Vial 10 DOSIS - SKBIOSCIENCE - KOREA",
-                ],
-                "Covishield": [
-                    "CHADOX1 NCOV-19 RECOMBINANTE - Vial 10 DOSIS - SERUM",
-                ],
-                "Sputnik V": [
-                    "GAM-COVID-VAC C1 - Vial 5 DOSIS - RICHMOND",
-                    "GAM-COVID-VAC C1 - Vial 2 DOSIS - BIOCAD-PHARM-UFAVITA",
-                    "GAM-COVID-VAC C1 - Vial 5 DOSIS - JSC R-PHARM",
-                    "GAM-COVID-VAC C1 - Vial 5 DOSIS - JSC BIOCAD",
-                    "GAM-COVID-VAC C1 - Vial 5 DOSIS - JSC GENERIUM",
-                    "GAM-COVID-VAC C1 - Vial 5 DOSIS - CJSC LEKKO",
-                    "GAM-COVID-VAC C1 - Vial 1 DOSIS - PHARMSTANDARD-UFAVITA",
-                    "GAM-COVID-VAC C2 - Vial 5 DOSIS - RICHMOND",
-                    "GAM-COVID-VAC C2 - Vial 2 DOSIS - GENERIUM-PHARM-UFAVITA",
-                    "GAM-COVID-VAC C2 - Vial 5 DOSIS - JSC BIOCAD",
-                    "GAM-COVID-VAC C2 - Vial 1 DOSIS - PHARMSTANDARD-UFAVITA",
-                    "GAM-COVID-VAC C2 - Vial 5 DOSIS - JSC GENERIUM",
-                    "GAM-COVID-VAC C2 - Vial 5 DOSIS - CJSC LEKKO",
-                    "GAM-COVID-VAC C2 - Vial 1 DOSIS - JSC BINNOPHARM",
-                ],
-                "Sputnik Light": [
-                    "SPUTNIK LIGHT SOLUCIÓN INYECTABLE (0,5 ML) - Vial 1 DOSIS - MED GAMALEYA"
-                ],
-                "Sinopharm": [
-                    "SARS-COV-2-VACCINE - Vial 1 DOSIS - BEIJING INSTITUTE",
-                    "SARS-COV-2-VACCINE - Vial 2 DOSIS - BEIJING INSTITUTE",
-                ],
-                "Cansino": [
-                    "CONVIDECIA RECOMBINANT NOVEL VACCINE - Vial 1 DOSIS - CANSINO",
-                    "CONVIDECIA RECOMBINANT NOVEL VACCINE - Vial 3 DOSIS - CANSINO",
-                ],
-                "Moderna": [
-                    "MODERNA COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,2 MG/ML) - Vial 10 DOSIS - MOD - CAT",
-                    "MODERNA COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,2 MG/ML) - Vial 14 DOSIS - LON-BAX PHARM",
-                    "MODERNA COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,2 MG/ML) - Vial 14 DOSIS - MOD - CAT",
-                    "MODERNA COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,2 MG/ML) - Vial 14 DOSIS - MOD-BAX PHARM",
-                ],
-                "Moderna Pediatrica": [
-                    "MODERNA Pediátrica COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,1 MG/ML) - Vial 10 DOSIS - CAT - MOD",
-                    "MODERNA Pediátrica COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,1 MG/ML) - Vial 10 DOSIS - ROV - MOD",
-                ],
-                "Moderna Bivariante": [
-                    "MODERNA BIVALENTE Original/Omicron COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,1 MG/ML) - Vial 5 DOSIS - MOD - CAT",
-                    "MODERNA BIVALENTE Original/Omicron COVID-19 (MRNA1273) DISPERSION INYECTABLE (0,1 MG/ML) - Vial 5 DOSIS - LON - ROV",
-                ],
-                "Pfizer": [
-                    "COVID-19 MRNA VACCINE BNT162B2 POLVO PARA INYECTABLE (30 MCG/DOSIS) - Vial 6 DOSIS - BIONTECH",
-                    "COVID-19 MRNA VACCINE BNT162B2 POLVO PARA INYECTABLE (30 MCG/DOSIS) - Vial 6 DOSIS - PFIZER",
-                ],
-                "Pfizer Pediatrica": [
-                    "COVID-19 MRNA VACCINE BNT162B2 PEDIATRICA POLVO PARA INYECTABLE (10 MCG/DOSIS) - Vial 10 DOSIS - PFIZER"
-                ],
-                "Pfizer Bivariante": [
-                    "COVID-19 MRNA BNT162B2 BIVALENTE Original/Omicron DISPERSION INYECTABLE (30 MCG/DOSIS) - Vial 6 DOSIS - BIONTECH",
-                    "COVID-19 MRNA BNT162B2 BIVALENTE Original/Omicron DISPERSION INYECTABLE (30 MCG/DOSIS) - Vial 6 DOSIS - PFIZER",
-                ],
-            }
+            global diccionario_de_vacunas
+            nombres_con_variantes = diccionario_de_vacunas
 
-            for nombre, nombres_variables in cambiar_nombre.items():
+            for nombre, nombres_variables in nombres_con_variantes.items():
                 for nombre_variable in nombres_variables:
                     if nombre_variable in item["Producto origen"]:
                         item["Producto origen"] = nombre
