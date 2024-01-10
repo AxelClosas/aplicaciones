461d493904c41de3b944301a94184d61ad5076e6
Axel Closas <axelclosas13@gmail.com>
2024-01-10 12:36:50 -0300

    Update MENU

--- menu.py
+++ menu.py
@@ -1,21 +1,15 @@
 def menu():
     menu = """
-oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-o                                                                o
-o        ooooooo    ooooooo    ooo  ooo    ooo    oooooo         o
-o        ooooooo    ooooooo    ooo  ooo    ooo    ooooooo        o
-o        ooo        oo   oo    ooo  ooo    ooo    oo    oo       o
-o        ooo        oo   oo    ooo  ooo    ooo    oo    oo       o
-o        ooo        oo   oo    ooo  ooo    ooo    oo    oo       o
-o        ooooooo    ooooooo     oo  oo     ooo    ooooooo        o
-o        ooooooo    ooooooo      oooo      ooo    oooooo         o
-o                                                                o
-oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-
-Hola!, este programa permite automatizar la extracción de datos en archivos CSV, por lo tanto, solo podrías ejecutar 1 tarea
-por cada ejecución del Script. A continuación tienes un menú de opciones con el número asociado a la tarea para realizar, por favor escribe
-la opción que desees y a continuación presiona Enter para iniciar la ejecución. Para conocer a fondo como trabaja el Script, por favor
-consulta el material "Explicacion_programa.docx".
+ _____                                                                           _____ 
+( ___ )-------------------------------------------------------------------------( ___ )
+ |   |                                                                           |   | 
+ |   |  ___                             _               _   __                   |   | 
+ |   | |_ _|_ __  _ __ ___  _   _ _ __ (_)______ _  ___(_) /_/  _ __   ___  ___  |   | 
+ |   |  | || '_ \\| '_ ` _ \\| | | | '_ \\| |_  / _` |/ __| |/ _ \\| '_ \\ / _ \\/ __| |   | 
+ |   |  | || | | | | | | | | |_| | | | | |/ / (_| | (__| | (_) | | | |  __/\\__ \\ |   | 
+ |   | |___|_| |_|_| |_| |_|\\__,_|_| |_|_/___\\__,_|\\___|_|\\___/|_| |_|\\___||___/ |   | 
+ |___|                                                                           |___| 
+(_____)-------------------------------------------------------------------------(_____)
 
     1. Primer reporte
         -> Total de vacunas aplicadas en general
@@ -29,5 +23,7 @@ consulta el material "Explicacion_programa.docx".
     3. Tercer reporte - Tipo y cantidad de Aplicaciones (1ra dosis, 2da dosis, etc, incluidos los refuerzos)
         -> Dosis aplicadas en general
         -> Dosis aplicadas a la población de Catamarca (Provincia de domicilio = Catamarca)
+
+    4. Cuarto reporte - Cantidad de dosis distribuidas por vacuna a cada Institución - DiCEI
     """
     return menu
